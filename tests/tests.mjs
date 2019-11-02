425abbac6802ca0101eadd9f9497574886c58e9c
Jakob Kummerow <jakob.kummerow@gmail.com>
2019-11-02 19:57:53 +0100

    Fix off-by-one in string parsing
    When there was whitespace and then a single character of non-whitespace
    trailing garbage after the BigInt, we failed to look at that last character.
    
    Fixes #36.

--- tests/tests.mjs
+++ tests/tests.mjs
@@ -46,6 +46,24 @@ const TESTS = [
   },
 ];
 
+// https://github.com/GoogleChromeLabs/jsbi/issues/36
+(function() {
+  const VALID = ['123', ' 123 ', '   123   '];
+  const INVALID = ['x123', 'x 123', ' 123x', '123 x', '123  xx', '123 ?a'];
+  for (const v of VALID) {
+    let result = JSBI.BigInt(v);
+    console.assert(JSBI.equal(result, JSBI.BigInt(123)));
+  }
+  for (const i of INVALID) {
+    try {
+      let result = JSBI.BigInt(i);
+      throw "unreachable";
+    } catch (e) {
+      console.assert(e instanceof SyntaxError);
+    }
+  }
+})()
+
 function parse(string) {
   if (string.charCodeAt(0) === 0x2D) { // '-'
     const result = JSBI.BigInt(string.slice(1));
