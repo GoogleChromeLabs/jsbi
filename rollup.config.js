add2f2fad55c47cecdb08252dba22a6612100709
James Wright <jameswr@google.com>
2021-09-06 12:45:00 -0700

    Fix UMD exports to be default, similar to https://github.com/GoogleChromeLabs/jsbi/pull/69.

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
-// Copyright 2018 Google Inc.
-//
+// Copyright 2018 Google Inc.  //
 // Licensed under the Apache License, Version 2.0 (the “License”);
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -59,7 +58,7 @@ export default [
         name: 'JSBI',
         file: pkg.browser,
         format: 'umd',
-        exports: 'named',
+        exports: 'default',
         sourcemap: true,
       },
     ],
