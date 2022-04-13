7dee7e79b7035858908d1920cdad32888d2e054e
Mathias Bynens <mathias@qiwi.be>
2022-04-13 19:59:55 +0200

    Configure tsc to emit jsbi.d.ts (#90)
    Fixes #89.

--- jsbi.d.ts
+++ jsbi.d.ts
@@ -1,44 +1,43 @@
-export default class JSBI {
-  private constructor(length: number, sign: boolean);
-  private length: number;
-  private sign: boolean;
-
-  static BigInt(from: number|string|boolean|object): JSBI;
-
-  toString(radix?: number): string;
-  static toNumber(x: JSBI): number;
-
-  static unaryMinus(x: JSBI): JSBI;
-  static bitwiseNot(x: JSBI): JSBI;
-
-  static exponentiate(x: JSBI, y: JSBI): JSBI;
-  static multiply(x: JSBI, y: JSBI): JSBI;
-  static divide(x: JSBI, y: JSBI): JSBI;
-  static remainder(x: JSBI, y: JSBI): JSBI;
-  static add(x: JSBI, y: JSBI): JSBI;
-  static subtract(x: JSBI, y: JSBI): JSBI;
-  static leftShift(x: JSBI, y: JSBI): JSBI;
-  static signedRightShift(x: JSBI, y: JSBI): JSBI;
-
-  static lessThan(x: JSBI, y: JSBI): boolean;
-  static lessThanOrEqual(x: JSBI, y: JSBI): boolean;
-  static greaterThan(x: JSBI, y: JSBI): boolean;
-  static greaterThanOrEqual(x: JSBI, y: JSBI): boolean;
-  static equal(x: JSBI, y: JSBI): boolean;
-  static notEqual(x: JSBI, y: JSBI): boolean;
-
-  static bitwiseAnd(x: JSBI, y: JSBI): JSBI;
-  static bitwiseXor(x: JSBI, y: JSBI): JSBI;
-  static bitwiseOr(x: JSBI, y: JSBI): JSBI;
-
-  static asIntN(n: number, x: JSBI): JSBI;
-  static asUintN(n: number, x: JSBI): JSBI;
-
-  static ADD(x: any, y: any): any;
-  static LT(x: any, y: any): boolean;
-  static LE(x: any, y: any): boolean;
-  static GT(x: any, y: any): boolean;
-  static GE(x: any, y: any): boolean;
-  static EQ(x: any, y: any): boolean;
-  static NE(x: any, y: any): boolean;
+declare class JSBI extends Array {
+    private sign;
+    constructor(length: number, sign: boolean);
+    static BigInt(arg: number | string | boolean | object): JSBI;
+    toDebugString(): string;
+    toString(radix?: number): string;
+    valueOf(): void;
+    static toNumber(x: JSBI): number;
+    static unaryMinus(x: JSBI): JSBI;
+    static bitwiseNot(x: JSBI): JSBI;
+    static exponentiate(x: JSBI, y: JSBI): JSBI;
+    static multiply(x: JSBI, y: JSBI): JSBI;
+    static divide(x: JSBI, y: JSBI): JSBI;
+    static remainder(x: JSBI, y: JSBI): JSBI;
+    static add(x: JSBI, y: JSBI): JSBI;
+    static subtract(x: JSBI, y: JSBI): JSBI;
+    static leftShift(x: JSBI, y: JSBI): JSBI;
+    static signedRightShift(x: JSBI, y: JSBI): JSBI;
+    static unsignedRightShift(): void;
+    static lessThan(x: JSBI, y: JSBI): boolean;
+    static lessThanOrEqual(x: JSBI, y: JSBI): boolean;
+    static greaterThan(x: JSBI, y: JSBI): boolean;
+    static greaterThanOrEqual(x: JSBI, y: JSBI): boolean;
+    static equal(x: JSBI, y: JSBI): boolean;
+    static notEqual(x: JSBI, y: JSBI): boolean;
+    static bitwiseAnd(x: JSBI, y: JSBI): JSBI;
+    static bitwiseXor(x: JSBI, y: JSBI): JSBI;
+    static bitwiseOr(x: JSBI, y: JSBI): JSBI;
+    static asIntN(n: number, x: JSBI): JSBI;
+    static asUintN(n: number, x: JSBI): JSBI;
+    static ADD(x: any, y: any): string | number | JSBI;
+    static LT(x: any, y: any): boolean;
+    static LE(x: any, y: any): boolean;
+    static GT(x: any, y: any): boolean;
+    static GE(x: any, y: any): boolean;
+    static EQ(x: any, y: any): boolean;
+    static NE(x: any, y: any): boolean;
+    static DataViewGetBigInt64(dataview: DataView, byteOffset: number, littleEndian?: boolean): JSBI;
+    static DataViewGetBigUint64(dataview: DataView, byteOffset: number, littleEndian?: boolean): JSBI;
+    static DataViewSetBigInt64(dataview: DataView, byteOffset: number, value: JSBI, littleEndian?: boolean): void;
+    static DataViewSetBigUint64(dataview: DataView, byteOffset: number, value: JSBI, littleEndian?: boolean): void;
 }
+export default JSBI;
